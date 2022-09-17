9ad44c88df23bb5564c8ead90812ba4ae4fa9222
nhh75 <nguyenhoanghai0507@gmail.com>
2022-09-17 16:43:13 +0700

    i

---
+++ src/main/java/com/example/testdeploynew/TestDeployNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.testdeploynew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDeployNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestDeployNewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testdeploynew/TestDeployNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testdeploynew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDeployNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
